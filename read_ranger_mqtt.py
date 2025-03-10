8b094da58355f4b7b21bddb320ab157d2ed7447e
Peter Mullen <ptrmu@outlook.com>
2025-03-09 21:31:09 -0700

    Add serial read.

--- read_ranger_mqtt.py
+++ read_ranger_mqtt.py
@@ -4,14 +4,18 @@ import datetime as dt
 import json  # To handle JSON encoding
 import logging
 import paho.mqtt.client as mqtt
+import pathlib as pl
 import pigpio
 import re
+import serial
+import time
 
 
 # Define a typed configuration object using @dataclass
 @dc.dataclass
 class Config:
     serial_gpio: str
+    serial_port: str
     baud_rate: int
     data_bits: int
     pattern: str
@@ -21,10 +25,12 @@ class Config:
     mqtt_user: str
     mqtt_password: str
     log_level: str
+    source_type: int
+    source_desc: str
+    sink_desc: str
 
 
 def get_config() -> Config:
-
     parser = argparse.ArgumentParser(
         description="MQTT and serial communication program",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -33,8 +39,12 @@ def get_config() -> Config:
     serial_group = parser.add_argument_group("Serial Communication Settings")
     serial_group.add_argument("--serial-gpio",
                               type=int,
-                              default=15,
-                              help="GPIO pin to connect to the serial device")
+                              default=-1,
+                              help="GPIO pin connected to the serial device")
+    serial_group.add_argument("--serial-port",
+                              type=str,
+                              default="",
+                              help="Serial port connected to the serial device")
     serial_group.add_argument("--baud-rate",
                               type=int,
                               default=9600,
@@ -80,9 +90,23 @@ def get_config() -> Config:
     # Parse the arguments
     args = parser.parse_args()
 
+    source_type = 0
+    source_desc = "unspecified"
+    if args.serial_gpio != -1:
+        source_type = 1
+        source_desc = f"GPIO {args.serial_gpio}"
+
+    elif args.serial_port != "":
+        source_type = 2
+        source_desc = f"serial port {args.serial_port}"
+
+    sink_desc = f"MQTT broker {args.mqtt_broker}:{args.mqtt_port} " + \
+                    f"on topic '{args.mqtt_topic}'"
+
     # Return a Config object with parsed arguments
     return Config(
         serial_gpio=args.serial_gpio,
+        serial_port=args.serial_port,
         baud_rate=args.baud_rate,
         data_bits=args.data_bits,
         pattern=args.pattern,
@@ -92,11 +116,14 @@ def get_config() -> Config:
         mqtt_user=args.mqtt_user,
         mqtt_password=args.mqtt_password,
         log_level=args.log_level,
+        source_type=source_type,
+        source_desc=source_desc,
+        sink_desc=sink_desc,
     )
 
 
 def get_logger(log_level: str) -> logging.Logger:
-    logger = logging.getLogger("my_logger")
+    logger = logging.getLogger(pl.Path(__file__).stem)
     logger.setLevel(log_level.upper())  # Set log level (e.g., DEBUG, INFO)
 
     # Add a console handler with formatting
@@ -118,7 +145,82 @@ def display_config(config: Config, logger: logging.Logger):
         logger.debug("\nConfiguration Summary:\n" + config_summary)
 
 
-def read_from_ranger(config: Config, logger: logging.Logger) -> dict | None:
+def read_from_ranger_uart(config: Config, logger: logging.Logger) -> dict | None:
+    if config.data_bits == 5:
+        bytesize = serial.FIVEBITS
+    elif config.data_bits == 6:
+        bytesize = serial.SIXBITS
+    elif config.data_bits == 7:
+        bytesize = serial.SEVENBITS
+    elif config.data_bits == 8:
+        bytesize = serial.EIGHTBITS
+    else:
+        logger.error(f"Invalid data bits: {config.data_bits}")
+        raise ValueError(f"Unsupported data bit value: {config.data_bits}")
+
+    try:
+        # Open the serial connection
+        ser = serial.Serial(config.serial_port,
+                            baudrate=config.baud_rate,  # Baud rate
+                            bytesize=bytesize,  # Data bits: 8
+                            parity=serial.PARITY_NONE,  # Parity: None
+                            stopbits=serial.STOPBITS_ONE,  # Stop bits: 1
+                            timeout=1)
+
+    except serial.SerialException as e:
+        logger.error(f"Error opening {config.source_desc}: {e}")
+        raise
+
+    buffer = b""  # Initialize empty buffer
+    match = None
+    unix_time = None
+
+    # Define a timeout mechanism to prevent indefinite looping
+    start_time = time.time()
+    timeout = 20  # seconds
+
+    time.sleep(1)
+
+    try:
+        ser.read(ser.in_waiting) # flush buffer
+        while time.time() - start_time < timeout:
+            if ser.in_waiting > 0:  # Proceed only if data is available
+                data = ser.read(ser.in_waiting)
+                unix_time = int(dt.datetime.now(dt.timezone.utc).timestamp())
+                logger.debug(f"Read {len(data)} bytes: {data}")
+
+                buffer += data  # Append data to the buffer
+
+                # Check if buffer contains desired delimiter
+                if b'\r' in buffer:
+                    line, _, buffer = buffer.partition(b'\r')  # Partition buffer
+                    line = line.decode("utf-8", errors="replace").strip()
+                    logger.debug(f"Decoded line: {line}")
+
+                    # Match the pattern
+                    match = re.match(config.pattern, line)
+                    if match:
+                        break
+            time.sleep(0.1)
+
+        else:
+            logger.error(f"Serial reading timed out from {config.source_desc}")
+            raise
+
+    finally:
+        ser.close()
+
+    if match and match.groups():
+        return {
+            "timestamp": unix_time,
+            "ranger_distance": match.group(1),
+        }
+    else:
+        logger.error(f"Unexpected data format or regex match failed from {config.source_desc}.")
+        raise
+
+
+def read_from_ranger_gpio(config: Config, logger: logging.Logger) -> dict | None:
     """
     Reads multiple bytes into a buffer until a '\r'.
     Checks if the buffer matches a predefined pattern.
@@ -135,7 +237,6 @@ def read_from_ranger(config: Config, logger: logging.Logger) -> dict | None:
         pi.bb_serial_invert(config.serial_gpio, 1)  # Inverted mode flag
 
         buffer = b""  # Initialize empty buffer
-        digits = ""
         unix_time = 0
 
         while True:
@@ -161,8 +262,7 @@ def read_from_ranger(config: Config, logger: logging.Logger) -> dict | None:
                         }
 
     except Exception as e:
-        logger.error(f"Error in read_from_ranger (GPIO: {config.serial_gpio}, "
-                     f"Baud: {config.baud_rate}, Bits: {config.data_bits}): {e}")
+        logger.error(f"Error in read_from_ranger from {config.source_desc}: {e}")
         raise
 
     finally:
@@ -174,6 +274,18 @@ def read_from_ranger(config: Config, logger: logging.Logger) -> dict | None:
             except Exception as e:
                 logger.error(f"Error during pigpio cleanup: {e}")
                 raise
+
+
+def read_from_ranger(config: Config, logger: logging.Logger) -> dict | None:
+    if config.serial_gpio != -1:
+        return read_from_ranger_gpio(config, logger)
+
+    if config.serial_port != "":
+        return read_from_ranger_uart(config, logger)
+
+    logger.error(f"Error in read_from_ranger: Must specify - GPIO (--serial_gpio 18) or "
+                 "Port (--serial_port /dev/ttyS0) {config.serial_gpio}")
+    raise
 
 
 def send_to_mqtt(config: Config, payload_dict, logger: logging.Logger):
@@ -190,23 +302,20 @@ def send_to_mqtt(config: Config, payload_dict, logger: logging.Logger):
 
         # Publish to the MQTT topic
         mqtt_client.publish(config.mqtt_topic, payload_json)
-        logger.info(f"Published data from GPIO {config.serial_gpio} "
-                    f"to MQTT broker {config.mqtt_broker}:{config.mqtt_port} "
-                    f"on topic '{config.mqtt_topic}': {payload_json}")
+        logger.info(f"Published data from {config.source_desc} "
+                    f"to {config.sink_desc}': {payload_json}")
 
     except Exception as e:
-        logger.error(f"Error publishing to MQTT broker '{config.mqtt_broker}:{config.mqtt_port}' "
-                     f"on topic '{config.mqtt_topic}': {e}")
+        logger.error(f"Error publishing to {config.sink_desc}: {e}")
         raise
 
     finally:
         # Disconnect from the MQTT broker
         try:
             mqtt_client.disconnect()
-            logger.debug(f"Disconnected from MQTT broker '{config.mqtt_broker}:{config.mqtt_port}'.")
+            logger.debug(f"Disconnected from {config.sink_desc}.")
         except Exception as e:
-            logger.error(f"Error during MQTT client cleanup for broker "
-                         f"'{config.mqtt_broker}:{config.mqtt_port}': {e}")
+            logger.error(f"Error during cleanup for {config.sink_desc}: {e}")
             raise
 
 
@@ -221,7 +330,7 @@ def main():
     if result and config.mqtt_broker and config.mqtt_port:
         send_to_mqtt(config, result, logger)
     else:
-        logger.info(f"No MQTT broker setup for data from GPIO {config.serial_gpio}: {result}")
+        logger.info(f"No MQTT broker setup for data from {config.source_desc }: {result}")
 
 
 if __name__ == "__main__":
