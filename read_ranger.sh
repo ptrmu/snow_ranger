572d3d0975cd48d281c21f8f196bfe1c90b907b3
Peter Mullen <ptrmu@outlook.com>
2025-04-06 17:43:11 -0700

    Small

--- read_ranger.sh
+++ read_ranger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 current_date=$(date +"%Y%m%d")
-python3 ~/Work/SnowRanger/read_ranger_mqtt.py --mqtt-user peter --mqtt-password carmal --serial-gpio 18 &>> ~/Work/SnowRanger/rm_$current_date.log
-python3 ~/Work/SnowRanger/read_ranger_send_mqtt.py --mqtt-user peter --mqtt-password carmal --serial-port /dev/ttyS0 --mqtt-topic snowdata/921a_15 &>> ~/Work/SnowRanger/rs_$current_date.log
+python3 ~/Work/SnowRanger/read_ranger_mqtt.py --mqtt-user peter --mqtt-password 46289 --serial-gpio 18 &>> ~/Work/SnowRanger/rm_$current_date.log
+python3 ~/Work/SnowRanger/read_ranger_send_mqtt.py --mqtt-user peter --mqtt-password 46289 --serial-port /dev/ttyS0 --mqtt-topic snowdata/921a_15 &>> ~/Work/SnowRanger/rs_$current_date.log
